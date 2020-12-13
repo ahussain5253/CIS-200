c6cfb1cea12f152867ff425f9a1b236dbe9f8bcc
Ammar <ammarh786@gmail.com>
2020-12-13 00:34:00 -0500

    final sub

--- Project 3/Project 3/LFUCache.h
+++ Project 3/Project 3/LFUCache.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Cache.h"
+
+class LFUCache : public Cache {
+
+private:
+
+	int* frequency;
+
+public:
+
+	LFUCache(int size);
+	void addItem(int item);
+	int getIndexOfItem(int item);
+
+};
